090eefd7e17cc7f2902924e8af622b67ecc280ca
직두 <pocard0117@naver.com>
2022-07-24 10:53:21 +0000

    왕실의 나이트 문제 미완

--- .Boj/왕실의 나이트.py
+++ .Boj/왕실의 나이트.py
@@ -1,5 +1,14 @@
 place=input()
 x,y=place[:1],place[1:]
-
+cnt=0
 steps=[(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
 
+for i in range(steps):
+    dx=x+steps[i][0]
+    dy=y+steps[i][1]
+    if dx<1 or dx>8 or dy<1 or dy>8:
+        continue
+    else:
+        cnt+=1
+print(cnt)
+    
