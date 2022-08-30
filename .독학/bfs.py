c70cf8eebcdbd297bc42231ed07f67e1a730e3dd
직두 <pocard0117@naver.com>
2022-08-30 10:35:49 +0000

    dfs 첫 구현이다.

--- .독학/bfs.py
+++ .독학/bfs.py
@@ -1,16 +1,14 @@
-def dfs(graph, v,visited):
-    visited[v]=True
-    print(v,end=' ')
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+from collections import deque
 
-graph=[
-    [],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]
-]
+def bfs(graph, start, visited):
+    queue= deque([start])
 
-visited=[False]*9
+    visited[start]=True
 
-dfs(graph, 1, visited)
-
-#처음써본 dfs구현
+    while queue:
+        v=queue.popleft()
+        print(v,end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i]=True
---
+++ .독학/음료수 얼려 먹기.py
@@ -0,0 +1,28 @@
+from sys import stdin
+
+n,m=map(int, stdin.readline().split())
+
+ice=[]
+for i in range(n):
+    ice.append(list(map(int,stdin.readline().strip())))
+
+def dfs(x,y):
+    if x<=-1 or x>=n or y<=-1 or y>=m:
+        return False
+    if ice[x][y]==0:
+        ice[x][y]=1
+        dfs(x,y-1)
+        dfs(x,y+1)
+        dfs(x-1,y)
+        dfs(x+1,y)
+        return True
+    return False
+
+cnt=0
+
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j)==True:
+            cnt+=1
+
+print(cnt)
