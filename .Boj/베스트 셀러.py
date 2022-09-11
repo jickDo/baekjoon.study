709c08b742926402b125317c252d8629c4df55bf
직두 <pocard0117@naver.com>
2022-09-11 06:57:15 +0000

    위 문제는 백준 1302번 실버4문제이다. 위문제 같은 경우는 colletions의 couner만 알고있다면 쉽게 풀 수 있다.

---
+++ .Boj/베스트 셀러.py
@@ -0,0 +1,12 @@
+from sys import stdin
+from collections import Counter
+n=int(stdin.readline().strip())
+
+arr=[]
+for i in range(n):
+    arr.append(stdin.readline().strip())
+
+arr.sort()
+res=Counter(arr).most_common(1)
+
+print(res[0][0])
