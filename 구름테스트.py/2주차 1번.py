fb2055cf35abecdbfd7ab0aff3d7eb94b5b330b0
직두 <pocard0117@naver.com>
2022-10-20 11:25:34 +0000

    위 문제는 구름 테스트 2주차 1번 문제이다.

---
+++ 구름테스트.py/2주차 1번.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+
+t = int(input())
+for _ in range(t):
+    
+    n = int(input())
+    arr = list(map(int, input().split()))
+    avg = sum(arr) / n
+    cnt = 0
+    for s in arr:
+        if s >= avg:
+            cnt += 1
+    print("%d/%d"%(cnt, n))
